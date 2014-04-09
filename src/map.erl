94be0ca134f00c8ac78486df84d6b9f2f347d979
BartAdv <bprzygoda@gmail.com>
2014-04-10 00:29:35 +0200

    Better monitor/demonitor handling for map critters

--- src/map.erl
+++ src/map.erl
@@ -4,7 +4,9 @@
 -export([add_critter/2, remove_critter/2]).
 -export([init/1, terminate/2, start_link/0, handle_call/3, handle_info/2]).
 
--record(map_data, {critters=sets:new()}).
+-export([test/0]).
+
+-record(map_data, {critters=gb_trees:empty()}).
 
 %% API
 add_critter(Pid, Cr) ->
@@ -15,8 +17,14 @@ remove_critter(Pid, Cr) ->
 
 %% internals
 
+map_add_critter(#map_data{critters=Crits}=Map, Cr) ->
+    Ref = monitor(process, Cr),
+    Map#map_data{critters=gb_trees:insert(Cr, {critter, Ref}, Crits)}.
+
 map_remove_critter(#map_data{critters=Crits}=Map, Cr) ->
-    Map#map_data{critters = sets:del_element(Cr, Crits)}.
+    {critter, Ref} = gb_trees:get(Cr, Crits),
+    demonitor(Ref),
+    Map#map_data{critters = gb_trees:delete(Cr, Crits)}.
 
 %% Server
 
@@ -29,13 +37,23 @@ init(_Args) ->
 terminate(_Reason, _) ->
     ok.
 
-handle_call({add_critter, Cr}, _From, #map_data{critters=Crits}=Map) ->
-    monitor(process, Cr),
-    {reply, ok, Map#map_data{critters=sets:add_element(Cr, Crits)}};
+handle_call({add_critter, Cr}, _From, Map) ->
+    {reply, ok, map_add_critter(Map, Cr)};
 
 handle_call({remove_critter, Cr}, _From, Map) ->
     {reply, ok, map_remove_critter(Map, Cr)}.
 
 %% when Critter process dies, we need to remove our references to it
-handle_info({'DOWN', _Ref, process, Cr, _Reason}, Map) ->
-    {noreply, map_remove_critter(Map, Cr)}.
+handle_info({'DOWN', Ref, process, Pid, _Reason}, #map_data{critters=Crits}=Map) ->
+    case gb_trees:lookup(Pid, Crits) of
+        {value, {critter, Ref}} ->
+            {noreply, map_remove_critter(Map, Pid)}
+        end.
+
+test() ->
+    Cr = spawn(fun() -> receive kill -> exit(ok) end end),
+    {ok, Pid} = map:start_link(),
+    map:add_critter(Pid, Cr),
+    map:remove_critter(Pid, Cr),
+    map:add_critter(Pid, Cr),
+    Cr ! kill.
