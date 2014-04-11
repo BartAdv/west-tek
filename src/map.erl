b94a66b845da3e4109efeb6fd7a5293d8816529b
BartAdv <bprzygoda@gmail.com>
2014-04-12 00:37:26 +0200

    No need for artificial item/critter division taken from original. Entity system is planned anyway

--- src/map.erl
+++ src/map.erl
@@ -1,21 +1,21 @@
 -module(map).
 -behaviour(gen_server).
 
--export([add_critter/2, remove_critter/2]).
+-export([add_entity/2, remove_entity/2]).
 -export([init/1, terminate/2, start_link/0, handle_call/3, handle_info/2]).
 -export([add_handler/3, call/3, notify/2]).
 
 -export([test/0]).
 
--record(map_data, {critters=gb_trees:empty(),
+-record(map_data, {entities=gb_trees:empty(),
                    event_mgr}).
 
 %% API
-add_critter(Pid, Cr) ->
-    gen_server:call(Pid, {add_critter, Cr}).
+add_entity(Pid, Cr) ->
+    gen_server:call(Pid, {add_entity, Cr}).
 
-remove_critter(Pid, Cr) ->
-    gen_server:call(Pid, {remove_critter, Cr}).
+remove_entity(Pid, Cr) ->
+    gen_server:call(Pid, {remove_entity, Cr}).
 
 add_handler(Pid, Handler, Args) ->
     gen_server:call(Pid, {add_handler, Handler, Args}).
@@ -28,14 +28,14 @@ notify(Pid, Event) ->
 
 %% internals
 
-map_add_critter(#map_data{critters=Crits}=Map, Cr) ->
-    Ref = monitor(process, Cr),
-    Map#map_data{critters=gb_trees:insert(Cr, {critter, Ref}, Crits)}.
+map_add_entity(#map_data{entities=Es}=Map, Ent) ->
+    Ref = monitor(process, Ent),
+    Map#map_data{entities=gb_trees:insert(Ent, Ref, Es)}.
 
-map_remove_critter(#map_data{critters=Crits}=Map, Cr) ->
-    {critter, Ref} = gb_trees:get(Cr, Crits),
+map_remove_entity(#map_data{entities=Es}=Map, Ent) ->
+    Ref = gb_trees:get(Ent, Es),
     demonitor(Ref),
-    Map#map_data{critters = gb_trees:delete(Cr, Crits)}.
+    Map#map_data{entities = gb_trees:delete(Ent, Es)}.
 
 %% Server
 
@@ -49,11 +49,11 @@ init(_Args) ->
 terminate(_Reason, _) ->
     ok.
 
-handle_call({add_critter, Cr}, _From, Map) ->
-    {reply, ok, map_add_critter(Map, Cr)};
+handle_call({add_entity, Ent}, _From, Map) ->
+    {reply, ok, map_add_entity(Map, Ent)};
 
-handle_call({remove_critter, Cr}, _From, Map) ->
-    {reply, ok, map_remove_critter(Map, Cr)};
+handle_call({remove_entity, Ent}, _From, Map) ->
+    {reply, ok, map_remove_entity(Map, Ent)};
 
 handle_call({notify, Event}, _From, #map_data{event_mgr=EventMgr}=Map) ->
     gen_event:notify(EventMgr, Event),
@@ -68,17 +68,17 @@ handle_call({add_handler, Handler, Args}, _From, #map_data{event_mgr=EventMgr}=M
     {reply, Res, Map}.
 
 
-%% when Critter process dies, we need to remove our references to it
-handle_info({'DOWN', Ref, process, Pid, _Reason}, #map_data{critters=Crits}=Map) ->
-    case gb_trees:lookup(Pid, Crits) of
-        {value, {critter, Ref}} ->
-            {noreply, map_remove_critter(Map, Pid)}
+%% when Entity process dies, we need to remove our references to it
+handle_info({'DOWN', Ref, process, Pid, _Reason}, #map_data{entities=Es}=Map) ->
+    case gb_trees:lookup(Pid, Es) of
+        {value, Ref} ->
+            {noreply, map_remove_entity(Map, Pid)}
         end.
 
 test() ->
     Cr = spawn(fun() -> receive kill -> exit(ok) end end),
     {ok, Pid} = map:start_link(),
-    map:add_critter(Pid, Cr),
-    map:remove_critter(Pid, Cr),
-    map:add_critter(Pid, Cr),
+    map:add_entity(Pid, Cr),
+    map:remove_entity(Pid, Cr),
+    map:add_entity(Pid, Cr),
     Cr ! kill.
