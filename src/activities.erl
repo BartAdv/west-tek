9110468fb2b6b3d94b0c67958c815fae3983ac07
BartAdv <bprzygoda@gmail.com>
2014-05-11 17:57:44 +0200

    Activities operating on processes

--- src/activities.erl
+++ src/activities.erl
@@ -1,47 +1,38 @@
 -module(activities).
 
 -export([enter_map/3, exit_map/2]).
-
+ 
 -define(TEST, 1).
-
+ 
 -ifdef(TEST).
 -export([test_entity/0, test_map/0]).
 -include_lib("eunit/include/eunit.hrl").
 -endif.
 
-enter_map(EntityId, MapId, Coords) ->
-    Entity = entity_mgr:get(EntityId),
-    Map = map_mgr:get(MapId),
+enter_map(Entity, Map, Coords) ->
     map:add_entity(Map, Entity),
     map:notify(Map, {entity_entered_map, Entity, Coords, Map}).
 
-exit_map(EntityId, MapId) ->
-    Entity = entity_mgr:get(EntityId),
-    Map = map_mgr:get(MapId),
+exit_map(Entity, Map) ->
     map:remove_entity(Map, Entity),
     map:notify(Map, {entity_left_map, Entity, Map}).
+
+move_on_map(Entity, Map, From, To) ->
+    map:notify(Map, {entity_moved_on_map, Entity, From, To, Map}).
 
 -ifdef(TEST).
 
 test_entity() ->
-    entity:start().
+    {ok, Pid} = entity:start(),
+    Pid.
 
 test_map() ->
-    map:start().
-
-i_dont_yet_know_how_to_make_eunit_fixtures_test() ->
-    application:start(gproc).
-
-test_init() ->
-    gproc:goodbye(),
-    entity_mgr:start_link(),
-    map_mgr:start_link(),
-    entity_mgr:add(1, activities, test_entity, []),
-    map_mgr:add(1, activities, test_map, []).
+    {ok, Pid} = map:start(),
+    Pid.
 
 enter_map_test() ->
-    test_init(),
-    enter_map(1, 1, {2,2}),
-    #{entities_count:=1} = map:get_info(map_mgr:get(1)).
+    Map = test_map(),
+    enter_map(test_entity(), Map, {2,2}),
+    #{entities_count:=1} = map:get_info(Map).
 
 -endif.
